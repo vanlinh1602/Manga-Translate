7aaddf493a3403153c900af5cfc983ca1f6d9019
Nguyễn Văn Linh <vanlinh14121@gmail.com>
2023-05-18 15:14:23 +0700

    up

--- src/features/translate/index.tsx
+++ src/features/translate/index.tsx
@@ -1,11 +1,30 @@
-import { Layout } from 'antd';
-import React from 'react';
+import { Button, Image, Layout } from 'antd';
+import React, { useState } from 'react';
+import { backendService } from 'services';
 
-const Translate = () => (
-  <Layout>
-    <Layout.Header>Translate Image</Layout.Header>
-    <Layout.Content>Manga Translate</Layout.Content>
-  </Layout>
-);
+const Translate = () => {
+  const [img, setImg] = useState<string>();
+  return (
+    <Layout>
+      <Layout.Header>Translate Image</Layout.Header>
+      <Layout.Content>
+        Manga Translate
+        <Image width={500} src={img} />
+        <Button
+          onClick={async () => {
+            const image = await backendService.post<{ name: string; src: string }>('/get-image', {
+              name: 'Linh',
+            });
+            if (image.kind === 'ok') {
+              setImg(image.data.src as string);
+            }
+          }}
+        >
+          Get Image
+        </Button>
+      </Layout.Content>
+    </Layout>
+  );
+};
 
 export default Translate;
